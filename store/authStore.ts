98e5f1569b206bc58c8e001e61e69fa6e0b0e05a
qentaianime-cmyk <qentaianime@gmail.com>
2026-05-26 06:40:48 +0530

    Rename partialState to partialize in authStore

--- store/authStore.ts
+++ store/authStore.ts
@@ -51,7 +51,7 @@ export const useAuthStore = create<AuthState>()(
     }),
     {
       name: 'famsaas-auth',
-      partialState: (s) => ({
+      partialize: (s)  => ({
         token: s.token,
         merchantId: s.merchantId,
         displayName: s.displayName,

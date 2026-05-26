5e9cc20d62c72af75006828dee1519770ba6f258
qentaianime-cmyk <qentaianime@gmail.com>
2026-05-26 17:03:45 +0530

    Replace Bell icon with ArrowsSplit icon

--- components/setup/Step1Welcome.tsx
+++ components/setup/Step1Welcome.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { motion } from 'framer-motion'
-import { ArrowRight, Lightning, QrCode, Bell, Webhook } from '@phosphor-icons/react'
+import { ArrowRight, Lightning, QrCode, Bell, ArrowsSplit } from '@phosphor-icons/react'
 import { StepShell } from './StepShell'
 import { useAuthStore } from '@/store/authStore'
 

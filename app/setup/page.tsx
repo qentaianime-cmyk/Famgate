8013fa9d77d9d180b066527130487857b4c5eabb
qentaianime-cmyk <qentaianime@gmail.com>
2026-05-28 12:02:07 +0530

    Update page.tsx

--- app/setup/page.tsx
+++ app/setup/page.tsx
@@ -1,11 +1,8 @@
 'use client'
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
-
 export default function SetupIndex() {
   const router = useRouter()
-  useEffect(() => {
-    router.replace('/setup/step/1')
-  }, [router])
+  useEffect(() => { router.replace('/setup/step/1') }, [router])
   return null
 }

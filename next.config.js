7244818d9fe1f594378050f672cb86d37a33fda7
qentaianime-cmyk <qentaianime@gmail.com>
2026-06-04 12:04:05 +0530

    Simplify remotePatterns configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'animerasaya.serv00.net',
-      },
-    ],
+    remotePatterns: [{ protocol: 'https', hostname: 'animerasaya.serv00.net' }],
   },
+  // Prevent scroll restoration jank on refresh
+  experimental: { scrollRestoration: false },
 }
-
 module.exports = nextConfig
